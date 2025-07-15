5e170e72917fc1b22b793bc3e5e5a87082800070
ali <ali_abde37@outlook.com>
2025-07-15 11:46:55 -0400

    updates

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {'message':'Hello World, Welcome to the FASTAPI tutorial'}
+
+@app.get("/about")
+def about():
+    return {'message':'CampusX is an educational platform where you can learn AI.'}
